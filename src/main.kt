73b0095b1152762237ab2a45d6a8ce3a49ae5327
still-nameless <67115556+still-nameless@users.noreply.github.com>
2021-06-11 11:11:53 +0200

    Changes by Marc

--- src/main.kt
+++ src/main.kt
@@ -1,2 +1,7 @@
-class main {
+fun main(){
+    val input : String = "6x + 3 = 9"
+    val lexer : Lexer = Lexer(input)
+    val parser : Parser = Parser()
+    val evaluator : Evaluator = Evaluator()
+    val EquationSolver : EquationSolver = EquationSolver()
 }
